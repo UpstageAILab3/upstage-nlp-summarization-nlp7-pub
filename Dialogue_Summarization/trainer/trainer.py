817c7bd2257c641fa16d19248a577252bfdddacd
sususushahaha <josuhan1433@gmail.com>
2024-09-09 10:03:56 +0900

    doc: add annotation

---
+++ Dialogue_Summarization/trainer/trainer.py
@@ -0,0 +1,115 @@
+from transformers import AutoTokenizer, BartForConditionalGeneration, BartConfig
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import Trainer, TrainingArguments
+from transformers import EarlyStoppingCallback
+import os
+import wandb
+from transformers import T5TokenizerFast, T5ForConditionalGeneration, T5Config
+from utils.metrics import compute_metrics
+# 학습을 위한 trainer 클래스와 매개변수를 정의합니다.
+# def load_tokenizer_and_model_for_train(config, device):
+#     print('-'*10, 'Load tokenizer & model', '-'*10,)
+#     print('-'*10, f'Model Name : {config["general"]["model_name"]}', '-'*10,)
+#     model_name = config['general']['model_name']
+
+    
+#     bart_config = BartConfig().from_pretrained(model_name)
+#     # tokenizer = spm.SentencePieceProcessor(model_file='spm.model')
+#     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
+#     generate_model = BartForConditionalGeneration.from_pretrained(config['general']['model_name'], config=bart_config)
+
+#     special_tokens_dict = {'additional_special_tokens': config['tokenizer']['special_tokens']}
+#     tokenizer.add_special_tokens(special_tokens_dict)
+
+#     generate_model.resize_token_embeddings(len(tokenizer))  # 사전에 special token을 추가했으므로 재구성 해줍니다.
+#     generate_model.to(device)
+#     print(generate_model.config)
+
+#     print('-'*10, 'Load tokenizer & model complete', '-'*10,)
+
+#     return generate_model, tokenizer
+
+
+## T5모델
+def load_tokenizer_and_model_for_train(config, device):
+    print('-'*10, 'Load tokenizer & model', '-'*10,)
+    print('-'*10, f'Model Name : {config["general"]["model_name"]}', '-'*10,)
+    model_name = config['general']['model_name']
+    model_config = T5Config.from_pretrained(model_name)
+    
+    tokenizer = T5TokenizerFast.from_pretrained(model_name)
+    special_tokens_dict={'additional_special_tokens':config['tokenizer']['special_tokens']}
+    tokenizer.add_special_tokens(special_tokens_dict)
+    
+    generate_model = T5ForConditionalGeneration.from_pretrained(config['general']['model_name'], config=model_config)
+    generate_model.resize_token_embeddings(len(tokenizer)) # 사전에 special token을 추가했으므로 재구성 해줍니다.
+    generate_model.to(device)
+    print(generate_model.config)
+
+    print('-'*10, 'Load tokenizer & model complete', '-'*10,)
+    return generate_model , tokenizer
+
+
+
+def load_trainer_for_train(config,generate_model,tokenizer,train_inputs_dataset,val_inputs_dataset):
+    print('-'*10, 'Make training arguments', '-'*10,)
+    # set training args
+    training_args = Seq2SeqTrainingArguments(
+                output_dir=config['general']['output_dir'], # model output directory
+                overwrite_output_dir=config['training']['overwrite_output_dir'],
+                num_train_epochs=config['training']['num_train_epochs'],  # total number of training epochs
+                learning_rate=config['training']['learning_rate'], # learning_rate
+                per_device_train_batch_size=config['training']['per_device_train_batch_size'], # batch size per device during training
+                per_device_eval_batch_size=config['training']['per_device_eval_batch_size'],# batch size for evaluation
+                warmup_ratio=config['training']['warmup_ratio'],  # number of warmup steps for learning rate scheduler
+                weight_decay=config['training']['weight_decay'],  # strength of weight decay
+                lr_scheduler_type=config['training']['lr_scheduler_type'],
+                optim =config['training']['optim'],
+                gradient_accumulation_steps=config['training']['gradient_accumulation_steps'],
+                evaluation_strategy=config['training']['evaluation_strategy'], # evaluation strategy to adopt during training
+                save_strategy =config['training']['save_strategy'],
+                save_total_limit=config['training']['save_total_limit'], # number of total save model.
+                fp16=config['training']['fp16'],
+                load_best_model_at_end=config['training']['load_best_model_at_end'], # 최종적으로 가장 높은 점수 저장
+                seed=config['training']['seed'],
+                logging_dir=config['training']['logging_dir'], # directory for storing logs
+                logging_strategy=config['training']['logging_strategy'],
+                predict_with_generate=config['training']['predict_with_generate'], #To use BLEU or ROUGE score
+                generation_max_length=config['training']['generation_max_length'],
+                do_train=config['training']['do_train'],
+                do_eval=config['training']['do_eval'],
+                report_to=config['training']['report_to'] # (선택) wandb를 사용할 때 설정합니다.
+            )
+
+    # (선택) 모델의 학습 과정을 추적하는 wandb를 사용하기 위해 초기화 해줍니다.
+    wandb.init(
+        # entity=config['wandb']['entity'],
+        project=config['wandb']['project'],
+        name=config['wandb']['name'],
+    )
+
+    # # (선택) 모델 checkpoint를 wandb에 저장하도록 환경 변수를 설정합니다.
+    os.environ["WANDB_LOG_MODEL"]="true"
+    os.environ["WANDB_WATCH"]="false"
+
+    # Validation loss가 더 이상 개선되지 않을 때 학습을 중단시키는 EarlyStopping 기능을 사용합니다.
+    MyCallback = EarlyStoppingCallback(
+        early_stopping_patience=config['training']['early_stopping_patience'],
+        early_stopping_threshold=config['training']['early_stopping_threshold']
+    )
+    print('-'*10, 'Make training arguments complete', '-'*10,)
+    print('-'*10, 'Make trainer', '-'*10,)
+
+    # Trainer 클래스를 정의합니다.
+    trainer = Seq2SeqTrainer(
+        model=generate_model, # 사용자가 사전 학습하기 위해 사용할 모델을 입력합니다.
+        args=training_args,
+        train_dataset=train_inputs_dataset,
+        eval_dataset=val_inputs_dataset,
+        compute_metrics = lambda pred: compute_metrics(config,tokenizer, pred),
+        callbacks = [MyCallback]
+    )
+    print('-'*10, 'Make trainer complete', '-'*10,)
+
+    return trainer
