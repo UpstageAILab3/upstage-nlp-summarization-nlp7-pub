1ea32cac328a231ff1abf57406ce859fd7275f88
sususushahaha <josuhan1433@gmail.com>
2024-09-11 10:19:22 +0900

    feat: su

---
+++ suhan/dataset/dataset.py
@@ -0,0 +1,59 @@
+# Train에 사용되는 Dataset 클래스를 정의합니다.
+from torch.utils.data import Dataset , DataLoader
+class DatasetForTrain(Dataset):
+    def __init__(self, encoder_input, decoder_input, labels, len):
+        self.encoder_input = encoder_input
+        self.decoder_input = decoder_input
+        self.labels = labels
+        self.len = len
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.encoder_input.items()} # item[input_ids], item[attention_mask]
+        item2 = {key: val[idx].clone().detach() for key, val in self.decoder_input.items()} # item2[input_ids], item2[attention_mask]
+        item2['decoder_input_ids'] = item2['input_ids']
+        item2['decoder_attention_mask'] = item2['attention_mask']
+        item2.pop('input_ids')
+        item2.pop('attention_mask')
+        item.update(item2) #item[input_ids], item[attention_mask] item[decoder_input_ids], item[decoder_attention_mask]
+        item['labels'] = self.labels['input_ids'][idx] #item[input_ids], item[attention_mask] item[decoder_input_ids], item[decoder_attention_mask], item[labels]
+        return item
+
+    def __len__(self):
+        return self.len
+
+# Validation에 사용되는 Dataset 클래스를 정의합니다.
+class DatasetForVal(Dataset):
+    def __init__(self, encoder_input, decoder_input, labels, len):
+        self.encoder_input = encoder_input
+        self.decoder_input = decoder_input
+        self.labels = labels
+        self.len = len
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.encoder_input.items()} # item[input_ids], item[attention_mask]
+        item2 = {key: val[idx].clone().detach() for key, val in self.decoder_input.items()} # item2[input_ids], item2[attention_mask]
+        item2['decoder_input_ids'] = item2['input_ids']
+        item2['decoder_attention_mask'] = item2['attention_mask']
+        item2.pop('input_ids')
+        item2.pop('attention_mask')
+        item.update(item2) #item[input_ids], item[attention_mask] item[decoder_input_ids], item[decoder_attention_mask]
+        item['labels'] = self.labels['input_ids'][idx] #item[input_ids], item[attention_mask] item[decoder_input_ids], item[decoder_attention_mask], item[labels]
+        return item
+
+    def __len__(self):
+        return self.len
+
+# Test에 사용되는 Dataset 클래스를 정의합니다.
+class DatasetForInference(Dataset):
+    def __init__(self, encoder_input, test_id, len):
+        self.encoder_input = encoder_input
+        self.test_id = test_id
+        self.len = len
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.encoder_input.items()}
+        item['ID'] = self.test_id[idx]
+        return item
+
+    def __len__(self):
+        return self.len
